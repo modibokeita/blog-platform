db674997e6a993078cdcd58f64919b7a802bdf70
modibo keita <93909081keita@gmail.com>
2024-12-08 18:37:55 +0200

    Get single post

--- front-end/src/SinglePage/SinglePage.jsx
+++ front-end/src/SinglePage/SinglePage.jsx
@@ -1,42 +1,49 @@
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
+import "./singlePage.css"
 
-const posts = [
-  {
-    id: 1,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-    img: "https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-  {
-    id: 2,
-    title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-    img: "https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  },
-  // Add more posts as needed
-];
-
+import User from "../img/vector-users-icon.jpg";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import {Link} from "react-router-dom";
+import Menu from "../Menu/Menu";
 function SinglePage() {
-  const { id } = useParams();
-  const [post, setPost] = useState(null);
-
-  useEffect(() => {
-    // Find the post with the matching id
-    const foundPost = posts.find((post) => post.id === parseInt(id));
-    setPost(foundPost);
-  }, [id]);
-
-  if (!post) {
-    return <div>Loading...</div>; // Display loading while the post is being fetched
-  }
 
   return (
     <div className="single-post">
-      <h1>{post.title}</h1>
-      <img src={post.img} alt={post.title} />
-      <p>{post.desc}</p>
-      <button>Read More</button>
+     <div className="content">
+      <img src="https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+     </div>
+     <div className="user">
+      <img src={User} alt="" />
+      <div className="info">
+        <span>Keita</span>
+        <p>Posted 2 days ago</p>
+      </div>
+      <div className="edit">
+        <Link to={`/write?edit=2`}>
+          <img src={Edit} alt="" />
+        </Link>
+        <Link>
+          <img src={Delete} alt="" />
+        </Link>
+      </div>
+     </div>
+     <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     </p>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque corrupti dolorum laudantium, iure nemo explicabo nostrum modi unde libero, accusamus eaque alias minus aperiam beatae numquam non ipsum? Quia, quis!
+     </p>
+     <h1>Other posts you may like</h1>
+      <Menu />
     </div>
   );
 }
